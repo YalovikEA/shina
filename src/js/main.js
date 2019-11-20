687d3cca9f6871d9eb6b188f10426116ddbbc1a8
YalovikEA <yalovikea@gmail.com>
2019-11-20 21:45:56 +0300

    added mobile version

--- src/js/main.js
+++ src/js/main.js
@@ -6,12 +6,14 @@ $(document).ready(function() {
     $('.owl-carousel').owlCarousel({
       loop: true,
       margin: 20,
-      // nav: true,
       responsive: {
         0: {
           items: 1,
         },
-        992: {
+        768: {
+          items: 2,
+        },
+        1024: {
           items: 3,
         },
       },
